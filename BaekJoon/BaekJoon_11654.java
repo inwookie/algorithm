9b2d5e63e1d440a1cab4fc0014329ef1af098336
inwookie <inwook@berkeley.edu>
2021-10-02 23:01:47 +0900

    Create BaekJoon_11654.java

---
+++ BaekJoon/BaekJoon_11654.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class BaekJoon_11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char input = scanner.next().charAt(0);
+
+        int ascii = (int) input;
+
+        System.out.print(ascii);
+
+        
+    }
+}
+
