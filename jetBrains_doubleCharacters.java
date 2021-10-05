75d479f283cdd23a57e78172d5f836dce5bcb198
inwookie <inwook@berkeley.edu>
2021-10-06 08:57:10 +0900

    Create jetBrains_doubleCharacters.java

---
+++ jetBrains_doubleCharacters.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class jetBrains_doubleCharacters {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+
+        for (int i = 0; i < word.length(); i++) {
+            System.out.print(word.charAt(i));
+            System.out.print(word.charAt(i));
+        }
+
+    }
+}
