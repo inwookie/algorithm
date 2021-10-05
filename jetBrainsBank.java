8c0bcd266cbe8bfa73c643586a06e8b81bf69484
inwookie <inwook@berkeley.edu>
2021-10-05 14:32:00 +0900

    Create jetBrainsBank.java

---
+++ jetBrainsBank.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class jetBrainsBank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double m = sc.nextInt();
+        double p = sc.nextInt();
+        int k = sc.nextInt();
+
+        sc.close();
+        
+        int t = 0;
+            
+        while (k > m) {
+            m += m * (1 * p / 100);  
+            t++;       
+        }
+        System.out.print(t); 
+    }
+}
