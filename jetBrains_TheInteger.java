01e26b7a62b67825646285eddb7bc3c8120b677f
inwookie <inwook@berkeley.edu>
2021-10-05 15:21:14 +0900

    Create jetBrains_TheInteger.java

---
+++ jetBrains_TheInteger.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class jetBrains_TheInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int sum = 0;
+        int exceed = 0;
+        
+        while (sc.hasNext()) {
+            int n = sc.nextInt(); 
+            if (n == 0) {
+                if (sum >= 1000) {
+                    exceed = 1000;
+                }
+                break;
+            } else if (sum >= 1000) {
+                exceed = 1000;
+            } else if (n >= 1000) {
+                exceed = 1000;
+                sum += n;
+            } else {
+                sum += n;
+            }
+        }
+        sum -= exceed; 
+        System.out.print(sum);
+    }
+}
