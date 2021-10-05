cc9f819949398f46be9cda631b0236504548b8bc
inwookie <inwook@berkeley.edu>
2021-10-05 20:32:55 +0900

    Create BaekJoon_1157.java

---
+++ BaekJoon/BaekJoon_1157.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BaekJoon_1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26]; 
+        String s = br.readLine().toUpperCase(); 
+        
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 65]++;
+        }
+        
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65); 
+            } else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.print(ch);
+    }
+}
